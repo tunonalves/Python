d3481372f7fca09a0b517947858341d8740b70e6
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-06-13 20:07:38 -0300

    condicionales

--- Condicionales.py
+++ Condicionales.py
@@ -1,9 +1,11 @@
 def evaluacion(nota):
 	valoracion = "NULL"
-	if(nota >= 5 and nota <= 7):
+	if(nota >= 5) and (nota <= 7):
 		valoracion= "Suspenso"
-	elif(nota>=8):
+	elif(nota>=8) and (nota <= 10):
 		valoracion = "Aprobado"
+	elif(nota>10):
+		valoracion = "Nota Incorrecta"
 	else:
 		valoracion = "Desaprobado"
 	return valoracion
