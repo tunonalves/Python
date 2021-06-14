e7a17a731353c53685598f67114e7ef03138ed9d
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-06-14 20:34:10 -0300

    adicionales

---
+++ Instrucciones Adicionales.py
@@ -0,0 +1,11 @@
+nombre="Federico Tuñon Alves"
+
+print(len(nombre))
+
+contador = 0
+
+for i in nombre:
+	if i == " ":
+		continue # Continua sin contar los espacios en blanco
+	contador+=1
+print(contador)
