cab6b3690a92c5a67cfe3f60e79be7325afa5cb7
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-06-13 23:46:05 -0300

    bucle

--- Bucle.py
+++ Bucle.py
@@ -7,3 +7,10 @@ while contador < 100:
 print("Fin")
 
 
+edad = int(input("Indroduce tu edad: "))
+while edad < 0:
+	print("Has introducido una edad negativa no validad")
+	edad=int(input("Introduce tu edad: "))
+print("Gracias")
+print("Edad = "+str(edad))
+
