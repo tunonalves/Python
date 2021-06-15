f7d4bf4b31981b473dbc2cef48df16ec13af5271
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-06-15 00:22:07 -0300

    excp

---
+++ Excepciones3.py
@@ -0,0 +1,10 @@
+import math
+
+def calculo_raiz_cuadrada(numero):
+	return math.sqrt(numero)
+
+numeroUsuario = (int(input("Numero: ")))
+
+print(calculo_raiz_cuadrada(numeroUsuario))
+
+print("FIN PROGRAMA")
