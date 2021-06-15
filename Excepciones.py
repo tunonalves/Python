87ebedef6211daa22e95d429ed728e5816463c43
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-06-14 23:37:20 -0300

    exception

--- Excepciones.py
+++ Excepciones.py
@@ -13,13 +13,14 @@ def divide(num1,num2):
 	except ZeroDivisionError:
 		print("Error divicion por 0")
 		return "Operacion erronea"
-	
-try:
+while True:		
+	try:
 
-	op1=(int(input("Introduce el primer número: ")))
-	op2=(int(input("Introduce el segundo número: ")))	
-except ValueError:
-	print("ERROR VALOR INCORRECTO")
+		op1=(int(input("Introduce el primer número: ")))
+		op2=(int(input("Introduce el segundo número: ")))
+		break	
+	except ValueError:
+		print("ERROR VALOR INCORRECTO")
 
 operacion=input("Introduce la operación a realizar (suma,resta,multiplica,divide): ")
 
