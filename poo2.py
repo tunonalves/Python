948f21b64c981d18fa40ac1dfcfaf73f357fa4d0
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-07-04 21:26:56 -0300

    poo

--- poo2.py
+++ poo2.py
@@ -1,28 +1,44 @@
 class Persona():
-	nombre = ""
-	apellido = ""
-	edad = 0
-	genero = ""
+	__nombre = ""
+	__apellido = ""
+	__edad = 0
+	__genero = ""
 
-	def __init__(self,nombre,apellido,edad,genero):
-		self.nombre = nombre
-		self.apellido = apellido
-		self.edad = edad
-		self.genero = genero
+	def __init__(self,nombre,apellido,genero):
+		self.__nombre = nombre
+		self.__apellido = apellido
+		
+		self.__genero = genero
+	
+	def setEdad(self,edad):
+		if(edad < 0 or edad >100):
+			return "Error en la edad"
+		else:
+			self.__edad = edad
+			return self.__edad
+
 
 	def habla(self):
-		return "La persona que se llama " + self.nombre + " esta hablando"
+		return "La persona que se llama " + self.__nombre + " esta hablando"
 	def camina(self):
-		return "La persona que se llama " + self.nombre + " esta caminando"
+		return "La persona que se llama " + self.__nombre + " esta caminando"
 	def getdatos(self):
-		return "Nombre: "+self.nombre+" Apellido: "+self.apellido + \
-			" Edad: "+str(self.edad) + " Genero: " + self.genero
+		return "Nombre: "+self.__nombre+" Apellido: "+self.__apellido + \
+			" Edad: "+str(self.__edad) + " Genero: " + self.__genero
 
-mipersona = Persona("Juan","Perez",34,"Varon")
+
+	
+
+
+
+
+mipersona = Persona("Juan","Perez","Varon")
+
+mipersona.setEdad(67)
 
 print(mipersona.getdatos())
 
-mipersona = Persona("Federico","Tuñon ALves",33,"Varon")
+mipersona.__nombre="Federico" #No funciona por el encapsulamiento
 
 print(mipersona.getdatos())
 
