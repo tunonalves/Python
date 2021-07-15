d7850880448ceb147a4c55ccebf515bfb69c35ce
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-07-14 22:37:08 -0300

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name="paquetecalculos",
+	version="1.0",
+	description="Paquete de Calculos Matematicos",
+	author="Federico",
+	author_email="tunonalves@gmail.com",
+	url="www.lala.com",
+	packages=["ModuloMatematico"]
+
+)
