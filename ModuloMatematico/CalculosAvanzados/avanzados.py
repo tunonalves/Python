39a09fb3393d794f00d037170abb0a884e8bff9c
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-07-14 22:15:45 -0300

    modulos

---
+++ ModuloMatematico/CalculosAvanzados/avanzados.py
@@ -0,0 +1,5 @@
+def potencia(base,exponente):
+	print("Resultado: ",base**exponente)
+
+def redondear(numero):
+	print("Resultado: ",round(numero))
---
+++ ModuloMatematico/CalculosBasicos/basicos.py
@@ -0,0 +1,8 @@
+def sumar(num1,num2):
+	print("Resultado: ",num1+num2)
+
+def restar(num1,num2):
+	print("Resultado: ",num1-num2)
+
+def multiplicar(num1,num2):
+	print("Resultado: ",num1*num2)
