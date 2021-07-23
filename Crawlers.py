80d07a49f911d3371605e59586f0264338f4deca
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-07-23 20:32:01 -0300

    var

--- Crawlers.py
+++ Crawlers.py
@@ -1,7 +1,28 @@
 import requests
+from bs4 import BeautifulSoup
 
 mireque = requests.get("https://www.latecla.info/actualidad")
+midoc = """
+	<html>
+		<body>
+			<p> Este es el primer parrafo </p>
+			
+			<p> Este es el segundo parrafo </p>
 
-print(mireque.status_code)
-print(mireque.headers)
-print(mireque.text)
+			<a> es un vinculo </a>
+
+		</body>
+	</html>
+
+"""
+
+docFinal = BeautifulSoup(midoc, "html.parser")
+
+for parrafo in docFinal.find_all("p"):
+	print(parrafo.text)
+
+#print(mireque.status_code)
+#print(mireque.headers)
+#print(mireque.text)
+
+print(docFinal)
