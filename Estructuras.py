97f6a5f7a580dd1d441ccd19d24f7a6e320e0268
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-09-26 23:24:15 -0300

    estructuras

---
+++ Estructuras.py
@@ -0,0 +1,8 @@
+sistema_solar = "Mercurio Venus Tierra Marte Jupiter Saturno Urano Neptuno Pluton"
+
+planetas = set()
+
+for planet in sistema_solar.split(" "):
+	planetas.add(planet)
+
+print(planetas)
