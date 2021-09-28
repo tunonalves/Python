c02597e08c91b79627cd9d4c8119eeea8fa1b3bc
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-09-28 00:05:01 -0300

    interface

--- Interfaces.py
+++ Interfaces.py
@@ -3,9 +3,19 @@ from tkinter import *
 raiz = Tk()
 
 raiz.title("INDEX")
-raiz.resizable(0,0)
+raiz.resizable(1,1)
 raiz.geometry("800x600")
 raiz.config(bg="darkgrey")
+
+miframe = Frame()
+
+miframe.pack()
+miframe.config(bg="gray")
+miframe.config(width="500",height="300")
+miframe.pack(side="top",anchor="n",fill="x")
+
+
+
 
 raiz.mainloop()
 
