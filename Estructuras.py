4f60733b6cc43dc3ff5839a59d3a807dd45918fb
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-09-27 21:20:13 -0300

    estruc

--- Estructuras.py
+++ Estructuras.py
@@ -1,3 +1,5 @@
+import queue
+
 sistema_solar = "Mercurio Venus Tierra Marte Jupiter Saturno Urano Neptuno Pluton"
 
 planetas = set()
@@ -7,3 +9,18 @@ for planet in sistema_solar.split(" "):
 
 print(planetas)
 print(len(planetas))
+
+#micola = queue.Queue()
+#micola = queue.LifoQueue()
+micola = queue.PriorityQueue()
+
+micola.put((3,"Argentina"))
+micola.put((4,"Uruguay"))
+micola.put((2,"Chile"))
+micola.put((1,"Brasil"))
+
+#print(micola.get())
+
+for elemento in micola.queue:
+	print(elemento)
+
